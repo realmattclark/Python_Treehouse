6a83e926078d0782b75bd52c73f49478fb151566
realmattclark <matt.clark5991@gmail.com>
2022-03-04 15:25:09 -0600

    add confirm and pseudo-code to main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+#global variables
 Ticket_Price = 10
 Seats_Remaining = 1000
 
-ticket_amount = int(input("How many tickets would you like to purchase?"))
-
+#update on how many tickets available
 print("There are {} tickets remaining".format(Seats_Remaining))
-print("Confirm ticket amount: {} tickets at {} dollars?".format(ticket_amount, Ticket_Price))
+
+#prompt user for number of tickets/continue statement
+order = int(input("How many tickets would you like to purchase? "))
+confirm = input("Confirm {} tickets at ${} Y/N?".format(order, Ticket_Price))
+    
+if confirm == 'y' or 'Y':
+    Seats_Remaining -= 1
+    print("Seats remaining: {} ".format(Seats_Remaining))
+
+elif confirm == 'n' or 'N':
+    print("Oh well")
+
