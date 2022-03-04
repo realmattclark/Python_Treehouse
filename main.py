5994a391446a4749c96e0f1b1d1858414788adcb
realmattclark <matt.clark5991@gmail.com>
2022-03-04 14:52:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ Seats_Remaining = 1000
 ticket_amount = int(input("How many tickets would you like to purchase?"))
 
 print("There are {} tickets remaining".format(Seats_Remaining))
-print("Confirm ticket amount: {} tickets at", Ticket_Price, "dollars?".format(ticket_amount))
+print("Confirm ticket amount: {} tickets at {} dollars?".format(ticket_amount, Ticket_Price))
